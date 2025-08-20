8e0363565754812cc1d974fe2d2a02b4f8e74583
Lampz <lampazaj@gmail.com>
2025-08-20 18:35:17 +0800

    Update plant.ts

--- services/plant.ts
+++ services/plant.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getPlants = async (): Promise<PlantLibrary[]> => {
   try {
     const response = await axios.get(
-      "https://perenual.com/api/v2/species-list?key=sk-xqpS68a28379effca11887"
+      `https://perenual.com/api/v2/species-list?key=${process.env.PLANT_API_KEY!}`
     );
 
     return response.data.data as PlantLibrary[];
